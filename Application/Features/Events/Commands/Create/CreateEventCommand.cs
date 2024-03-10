4f1101616c41f6f759d5a5fe2d5b390c45cc0c05
abdulhamit-dev <hmd62392@gmail.com>
2024-03-10 20:12:52 +0300

    Mediator added

---
+++ Application/Features/Events/Commands/Create/CreateEventCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Events.Commands.Create
+{
+    public class CreateEventCommand:IRequest<CreatedEventResponse>
+    {
+        public string Name { get; set; }
+
+        public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, CreatedEventResponse>
+        {
+            public Task<CreatedEventResponse> Handle(CreateEventCommand request, CancellationToken cancellationToken)
+            {
+                CreatedEventResponse response = new();
+                response.Name = request.Name;
+                response.Id = new Guid();
+                return null;
+              
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/EventsController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Events.Commands.Create;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : BaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody]CreateEventCommand createEventCommand)
+        {
+            var response=await Mediator.Send(createEventCommand);
+
+            return Ok(response);
+        }
+    }
+}
